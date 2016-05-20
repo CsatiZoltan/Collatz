d3a45d805948b5d6123d35eeb93c9cbde9735045
CsatiZoltan <csati_zoltan@freemail.hu>
2016-05-20 17:22:53 +0200

    Moved from the main directory to the /src dir.

---
+++ src/writeToFile.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void writeToFile(int* result, long nNumber, char* filename)
+{
+   /* Open a text file where we save the results */
+   FILE *fp;
+   fp = fopen(filename,"w+");
+   if (fp==NULL)
+     printf("File opening was not successful.");
+   /* Create the header */
+   fprintf(fp, "  Number of steps\n\n");
+   long iNumber;
+   for(iNumber=1; iNumber <= nNumber; iNumber++)
+      fprintf(fp, "  %d\n", result[iNumber-1]);
+   /* Close the file */
+   fclose(fp);
+}
