468fe7715cbf0c3e0ca93fd8ea1696b55b697d81
CsatiZoltan <csati_zoltan@freemail.hu>
2016-05-23 18:05:07 +0200

    Temporarily disabled file writing. Enable benchmarking.

--- src/main.c
+++ src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "collatz.h"
 #ifdef _OPENMP
    #include <omp.h>
 #else
    #define omp_get_thread_num() 0
 #endif
 
-//void writeToFile(int* result, long nNumber, char* filename);
-//int collatz(long n);
+void writeToFile(int* result, long nNumber, char* filename);
+void benchmark(long n);
 
+/* Interface for the computational routine and the file handling */
 int main(int argc, char* argv[])
 {
 	int nNumber;
@@ -17,14 +19,9 @@ int main(int argc, char* argv[])
 	else
 		nNumber = atoll(argv[1]);
 
-   /* Loop through some natural numbers */
-   long iNumber;
-   int* iter = malloc(nNumber*sizeof(int));
-   #pragma omp parallel for
-   for (iNumber=1; iNumber<=nNumber; iNumber++)
-   {
-      iter[iNumber-1] = collatz(iNumber);
-   }
-   writeToFile(iter, nNumber, "result.txt");   
-   free(iter);
+	benchmark(nNumber);
+   //long iNumber;
+   //int* iter = malloc(nNumber*sizeof(int));
+   //writeToFile(iter, nNumber, "result.txt");   
+   //free(iter);
 }
