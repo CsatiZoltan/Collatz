a3e28b0d039dec9d4c0194432c750b4292d4222f
CsatiZoltan <csati_zoltan@freemail.hu>
2016-05-23 17:58:15 +0200

    Iteration put into collatz.c to avoid function call overhead. Possibility to give the number of threads to run on.

--- src/collatz.c
+++ src/collatz.c
@@ -1,15 +1,27 @@
-int collatz(long n)
+#ifdef _OPENMP
+   #include <omp.h>
+#else
+   #define omp_get_thread_num() 0
+#endif
+#include "collatz.h"
+
+void collatz(long n, int* iter, int nThreads)
 {
-   int i = 0;
-   int isodd; /* 1 if n is odd, 0 if even */
-   while(n > 1)
-   {
-      isodd = n%2;
-      if (isodd)
-         n = 3*n+1;
-      else
-         n/=2;
-      i++;
-   }
-   return i;
+	long i;
+	int isodd; /* 1 if n is odd, 0 if even */
+	#pragma omp parallel for
+	for (i = 1; i <= n; i++)
+	{
+		omp_set_num_threads(nThreads);
+		while(n > 1)
+		{
+			isodd = n%2;
+			if (isodd)
+				n = 3*n+1;
+			else
+				n/=2;
+			i++;
+		}
+		iter[i - 1] = i;
+	}
 }
