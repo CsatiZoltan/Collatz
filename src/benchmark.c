b0addcd148870c10c85e397c939c693518129890
CsatiZoltan <csati_zoltan@freemail.hu>
2016-05-26 12:26:47 +0200

    Start value can be given, not just automatically 1. Deallocation is done.

--- src/benchmark.c
+++ src/benchmark.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include "collatz.h"
-#ifdef _OPENMP
-   #include <omp.h>
-#else
-   #define omp_get_thread_num() 0
-#endif
 
 
 /* Tests the scalability of OpenMP by performing the calculations 
    on different number of threads */
-double* benchmark(long nNumber)
+double* benchmark(int startNumber, int endNumber)
 {
 	/* Print the available number of threads */
 	int nThreads;
@@ -20,18 +15,20 @@ double* benchmark(long nNumber)
 	/* Run the test */
 	int iThread;
 	double timeStart, timeEnd;
+	int nNumbers = endNumber - startNumber + 1;
 	double* timings = malloc(nThreads * sizeof(double));;
-	int* iter = malloc(nNumber*sizeof(int));
+	int* iter = malloc(nNumbers*sizeof(int));
 	for (iThread = 1; iThread <= nThreads; iThread++)
 	{
 		omp_set_num_threads(iThread);
 		printf("Running the calculation on %d threads ...\n", iThread);
 		timeStart = omp_get_wtime();
-		collatz(nNumber, iter, iThread);
+		collatz(startNumber, endNumber, iter, iThread);
 		timeEnd = omp_get_wtime() - timeStart;
 		printf("  Calculation finished in %lf seconds.\n", timeEnd);
 		timings[iThread - 1] = timeEnd;
 	}
+	free(iter);
 	return timings;
 }
 
