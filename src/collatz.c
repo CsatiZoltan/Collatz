64a8bba568d29c715c45246db53b8f0f20cfd7f0
CsatiZoltan <csati_zoltan@freemail.hu>
2016-05-26 12:17:48 +0200

    Now the loop variables are private and the program works well.

--- src/collatz.c
+++ src/collatz.c
@@ -1,27 +1,24 @@
-#ifdef _OPENMP
-   #include <omp.h>
-#else
-   #define omp_get_thread_num() 0
-#endif
 #include "collatz.h"
 
-void collatz(long n, int* iter, int nThreads)
+void collatz(int startNumber, int endNumber, int* iter, int nThreads)
 {
-	long i;
+	int i, n, counter;
 	int isodd; /* 1 if n is odd, 0 if even */
-	#pragma omp parallel for
-	for (i = 1; i <= n; i++)
+	#pragma omp parallel for private(counter, n, isodd) /* idea from http://stackoverflow.com/questions/37437195/counting-does-not-work-properly-in-openmp */
+	for (i = startNumber; i <= endNumber; i++)
 	{
+		counter = 0;
+		n = i;
 		omp_set_num_threads(nThreads);
-		while(n > 1)
+		while (n > 1)
 		{
 			isodd = n%2;
 			if (isodd)
 				n = 3*n+1;
 			else
 				n/=2;
-			i++;
+			counter++;
 		}
-		iter[i - 1] = i;
+		iter[i - startNumber] = counter;
 	}
 }
